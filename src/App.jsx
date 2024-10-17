908c96ae0002972fc1bb39e5e40dc9de62cc2a41
Ogechukwu11 <nwobumogechukwu@gmail.com>
2024-10-17 04:42:52 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-import { Route, Routes } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import Login from './compoents/Login/Login';
-import Register from './compoents/Register/Register';
-import LogoLoad from './compoents/LoadLogo/LoadLogo';
-import Profile from './compoents/Profile/Profile';
-=======
 import styles from "./App.module.css";
 import { Route, Routes } from "react-router-dom";
 import { useEffect, useState } from "react";
@@ -16,7 +8,6 @@ import Home from "./compoents/pages/Home/Home";
 import AddPost from "./compoents/pages/addPost/addPost";
 import Profile from "./compoents/pages/Profile/Profile";
 
->>>>>>> 7bf810c7d461c5a8648809ffff6049fbd34ae767
 function App() {
   const [isMobileOrTablet, setIsMobileOrTablet] = useState(
     window.innerWidth < 500
@@ -33,29 +24,6 @@ function App() {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-<<<<<<< HEAD
-	return (
-		<>
-			{!isMobileOrTablet ? (
-				//! donot touch
-				<div className='start'>
-					<h1>Sorry, this app is only available on mobile</h1>
-					<h3>
-						Please visit the site on a mobiledevice to access the content.
-					</h3>
-				</div>
-			) : (
-				<Routes>
-					<Route path='/' element={<LogoLoad />} />
-					<Route path='/login' element={<Login />} />
-					<Route path='/register' element={<Register />} />
-					<Route path='/home' element={<h1>Home Page</h1>} />
-					<Route path='/profile' element={<Profile />}></Route>
-				</Routes>
-			)}
-		</>
-	);
-=======
   return (
     <>
       {!isMobileOrTablet ? (
@@ -78,6 +46,5 @@ function App() {
       )}
     </>
   );
->>>>>>> 7bf810c7d461c5a8648809ffff6049fbd34ae767
 }
 export default App;
